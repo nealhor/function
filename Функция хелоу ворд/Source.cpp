afd63efd61c96efe04f556b18364baf488bd7e37
DESKTOP-1ENL65S\Nealhor <nealhor@gmail.com>
2018-02-07 13:33:12 +0200

    Add project files.

---
+++ Функция хелоу ворд/Source.cpp
@@ -0,0 +1,14 @@
+#include <iostream>     
+using namespace std;
+
+void function_name()
+{
+	cout << "Oh, it's bad luck to be you" << endl;
+	system("pause");
+}
+
+int main()
+{
+	function_name(); // „B„„x„€„r „†„…„~„{„ˆ„y„y
+	return 0;
+}
